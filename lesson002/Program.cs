ecd5fa7905f95f00fa412ed6f2e98647a2ec52ef
Artem KHaritonov <ar.xaritonow2013@yandex.ru>
2023-03-06 20:53:16 +0300

    added info

--- lesson002/Program.cs
+++ lesson002/Program.cs
@@ -1,6 +1,6 @@
-﻿// Console.WriteLine("Введите ваше имя: "); WriteLine - ввод с переносом строки
-// string username = Console.ReadLine();    Write - ввод в одну стороку
-// Console.WriteLine("Привет, ");
+﻿// Console.WriteLine("Введите ваше имя: "); WriteLine - вывод с переносом строки
+// string username = Console.ReadLine();    Write - вывод в одну стороку
+// Console.WriteLine("Привет, ");           ReadLine - считать строку из терминала
 // Console.WriteLine(username);
 
 Console.Write("Введите ваше имя: ");
