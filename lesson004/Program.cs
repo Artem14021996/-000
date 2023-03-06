2cb79f806d93e16261f4f352cbdb895b75a25970
Artem KHaritonov <ar.xaritonow2013@yandex.ru>
2023-03-06 21:13:29 +0300

    Added new folder lesson004

---
+++ lesson004/Program.cs
@@ -0,0 +1,7 @@
+﻿// int numberA = 10;                     int - целые числа 
+// int numberB = 5;                      double -деление(числа деляся с остатком) вещественные числа
+// Console.WriteLine(numberA / numberB);
+
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
