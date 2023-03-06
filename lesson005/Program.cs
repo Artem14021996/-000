ddd529432ef88d81e1fb8b493c274046dd4e05c3
Artem KHaritonov <ar.xaritonow2013@yandex.ru>
2023-03-06 21:46:48 +0300

    Added new folder lesson005

---
+++ lesson005/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "лиза")
+{
+    Console.WriteLine("Ура, это же ЛИЗОЧЕК");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
+// ToLower - переводит все символы в строке в нижний регистр
