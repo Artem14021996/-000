462dd31206243d40470cffa2812766eb6d778e73
Artem KHaritonov <ar.xaritonow2013@yandex.ru>
2023-03-06 20:49:33 +0300

    added new info lesson002

--- lesson002/Program.cs
+++ lesson002/Program.cs
@@ -1,3 +1,9 @@
-﻿string username = Console.ReadLine();
-Console.WriteLine("Привет, ");
-Console.WriteLine(username);
+﻿// Console.WriteLine("Введите ваше имя: "); WriteLine - ввод с переносом строки
+// string username = Console.ReadLine();    Write - ввод в одну стороку
+// Console.WriteLine("Привет, ");
+// Console.WriteLine(username);
+
+Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
