ca5e335c581ac629290cfc05a908380e6d4af77b
Artem KHaritonov <ar.xaritonow2013@yandex.ru>
2023-03-08 10:34:14 +0300

    added new folder lesson006

---
+++ lesson006/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 1;
+int b = 3;
+int c = 4;
+int d = 8;
+int e = 10;
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+// Задача с гирями
