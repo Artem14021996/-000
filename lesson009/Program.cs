8512d1a8b72b6e199a63f972aa7eb8a2d6d41b54
Artem KHaritonov <ar.xaritonow2013@yandex.ru>
2023-03-08 13:15:43 +0300

    added new folder lesson009

---
+++ lesson009/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int Max(int a, int b, int c);
+{
+    int result = a;
+    if (b > result) result = b;
+    if (c > result) result = c;
+    return result;
+}
+
+//               0   1   2   3   4   5   6   7   8    номер массива
+int[] array = { 21, 42, 63, 47, 55, 36, 77, 78, 29 };
+// array[0] = 22;
+// Console.WriteLine(array[0]);
+
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine(result);
+
