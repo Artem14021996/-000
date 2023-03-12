9344186acffe8de5de9d30f5324e7a74061749a8
Artem KHaritonov <ar.xaritonow2013@yandex.ru>
2023-03-12 12:24:03 +0300

    added new info in folder lesson010

---
+++ lesson010/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 11, 52, 86, 65, 85, 54, 65, 87 };
+
+int n = array.Length;
+int find = 65;  //номер из массива
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console.WriteLine(index); //определяет индекс массива
+        break; //прервать, найдет только 1 индекс в массиве
+    }
+        //index = index + 1
+        index++;
+}
