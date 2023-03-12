af73709a08bfb51a6ddca1e98047d81c8babf4b2
Artem KHaritonov <ar.xaritonow2013@yandex.ru>
2023-03-12 21:36:29 +0300

    added new folder lesson013

---
+++ lesson013/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// string Method4(int count, string text)  //переименовать символ/текст меняестя везде
+// {
+//     int i = 0;
+//     string result = String.Empty;   //пустая строка можно обозначить как ""
+
+//     while(i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+
+//     return result;
+// }
+
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+string Method4(int count, string text)  //переименовать символ/текст меняестя везде
+{
+   
+    string result = String.Empty;   //пустая строка можно обозначить как ""
+    for(int i = 0; i < count; i++)  //в счетчике [for] можно собрать все вместе формула получается короче и красивее
+    {
+        result = result + text;
+    }
+
+    return result;
+}
+
+string res = Method4(10, "z");
+Console.WriteLine(res);
