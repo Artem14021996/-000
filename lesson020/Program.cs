a7eac1acae3eddf4e4480507ab84041d0f427881
Artem KHaritonov <ar.xaritonow2013@yandex.ru>
2023-03-18 17:39:14 +0300

    added new folder lesson020

---
+++ lesson020/Program.cs
@@ -0,0 +1,17 @@
+﻿// f(1) = 1
+// f(2) = 1
+// f(n) = f(n) + f(n-2)
+
+int Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+    //Console.WriteLine(Fibonacci(i));
+}
+
+
