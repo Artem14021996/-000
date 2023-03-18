2cb0203d317ca4a231f73a213645445879302d5f
Artem KHaritonov <ar.xaritonow2013@yandex.ru>
2023-03-18 17:17:54 +0300

    added new folder lesson019

---
+++ lesson019/Program.cs
@@ -0,0 +1,20 @@
+﻿//
+
+// int Factorial(int n) -интеджер не вывозит количество данных
+double Factorial(int n) // - первое число умножает на 10 в степени Е+(число)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1; //return - возврат
+    else return n * Factorial(n - 1);
+
+    
+}
+for(int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+    
+    //Console.WriteLine(Factorial(i));
+}
+
+
